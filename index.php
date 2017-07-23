7345a7cf246c197d30906d45e0fd5164ce6397fa
Dhruba Jyoti Paul <dhrubajyotipaul3@gmail.com>
2017-07-23 11:51:48 +0600

    First Commit

---
+++ index.php
@@ -0,0 +1,146 @@
+<html>
+
+
+  <head>
+      <link rel="icon" type="image/png" sizes="16x16" href="Images/codepic.png">
+      <title>paultech</title>
+
+      <!--meta tags-->
+      <meta name="author" content="Dhruba Jyoti Paul">
+      <meta name="keywords" content="nuke proliferation, Nuke, The end of the earth, History of Nukes, Modern Nukes">
+      <meta name="description" content="What is Nuke Proliferation? Why should we care about it? And what are some possible consequences of it?">
+
+      <!--External CSS-->
+      <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+
+
+
+
+
+  <body>
+
+
+
+
+<br><br>
+
+
+
+  <h1 style="text-align:center;">
+    Tech Portfolio
+  </h1>
+  <h3 style="text-align:center;">
+    - by Dhruba Jyoti Paul
+  </h3>
+
+
+
+
+  <?php
+  echo str_repeat("<br>",4);
+?>
+
+    <h3>Date : 19th March, 2017<br><br>
+    American International School Dhaka's first Student Lead Conference, lead by Dhruba Jyoti Paul.</h3>
+<a href="Images/Professional_Development_Workshop/PDW1.jpg"><img src="Images/Professional_Development_Workshop/PDW1.jpg" alt="Professional Developement by Dhruba" style="width:334px;height:228px;"></a>
+<a href="Images/Professional_Development_Workshop/PDW2.jpg"><img src="Images/Professional_Development_Workshop/PDW2.jpg" alt="Professional Developement by Dhruba" style="width:334px;height:228px;"><br></a>
+<a href="Images/Professional_Development_Workshop/PDW3.jpg"><img src="Images/Professional_Development_Workshop/PDW3.jpg" alt="Professional Developement by Dhruba" style="width:364px;height:228px;"></a>
+<a href="Images/Professional_Development_Workshop/PDW4.jpg"><img src="Images/Professional_Development_Workshop/PDW4.jpg" alt="Professional Developement by Dhruba" style="width:304px;height:228px;"></a>
+
+
+
+<?php
+echo str_repeat("<br>",8);
+?>
+
+  <h2>Date : 28th April, 2017<br><br>
+  Made my first PC.</h2>
+<!--<div style="margin-right:34%;">-->
+
+<div class="container" style="float: left; overflow: hidden;">
+  <img src="Images/My_First_PC/PC1.JPG" alt="Dhruba's First PC" style="width:334px;height:228px;" class="image">
+  <div class="overlay">
+    <div class="text">1. Fitting CPU with Motherboard.</div>
+  </div>
+</div>
+
+<div class="container" style="overflow: hidden;">
+  <img src="Images/My_First_PC/PC2.JPG" alt="Dhruba's First PC" style="width:334px;height:228px;" class="image">
+  <div class="overlay">
+    <div class="text" style="color:yellow;">2. Installing the RAM!!!</div>
+  </div>
+</div>
+
+<div class="container" style="float: left; overflow: hidden;">
+  <img src="Images/My_First_PC/PC3.JPG" alt="Dhruba's First PC" style="width:334px;height:228px;" class="image">
+  <div class="overlay">
+    <div class="text">3. Installing the Motherboard into the case.</div>
+  </div>
+</div>
+
+
+<div class="container" style="overflow: hidden;">
+  <img src="Images/My_First_PC/PC4.JPG" alt="Dhruba's First PC" style="width:334px;height:228px;" class="image">
+  <div class="overlay">
+    <div class="text">4. Motherboard all set to go!</div>
+  </div>
+</div>
+
+
+<div class="container" style="float: left; overflow: hidden;">
+  <img src="Images/My_First_PC/PC5.JPG" alt="Dhruba's First PC" style="width:334px;height:228px;" class="image">
+  <div class="overlay">
+    <div class="text">5. Wirings.</div>
+  </div>
+</div>
+
+<div class="container" style="overflow: hidden;">
+  <img src="Images/My_First_PC/PC7.JPG" alt="Dhruba's First PC" style="width:334px;height:228px;" class="image">
+  <div class="overlay">
+    <div class="text" style="color:yellow;">6. Full fledged PC made by Dhruba Jyoti Paul!!!</div>
+  </div>
+</div>
+
+
+
+
+<!--</div>-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <?php
+  echo str_repeat("<br>",8);
+  ?>
+
+    <!--ENDINGS!-->
+    <center><p><a href="mailto:dhrubajyotipaul3@gmail.com" class="Contact" style="color:red;"> Contact Me </a> </p></center>
+    <center><br><p> Tech Portfolio &#169; by Dhruba Jyoti Paul </p></br></center>
+
+
+
+  </body>
+
+
+</html>
